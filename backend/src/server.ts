db9b9f634f412019b33995232c72590c0db0851e
Mariomin23 <mario.minuesa@gmail.com>
2026-07-01 01:52:39 +0200

    feat: update server log message and add environment configuration files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Fit-PokéAPI Server running on port ${PORT}`);
+  console.log(`BurnOut API running on port ${PORT}`);
 });
